c756dd3e22a970cd9a4e94951a3dbac4686d69d7
Eric C. Anderson <eric.anderson@noaa.gov>
2017-07-07 22:44:48 -0700

    Add a list column with the indices of the missing loci
    I will be able to use this for a better z-score calculation.

--- R/count_missing_data.R
+++ R/count_missing_data.R
@@ -6,7 +6,8 @@
 #' hard-wired for diploids.
 #' @param D the data frame
 #' @param gen_start_col the column in which the genetic data starts
-#' @return returns a data frame with indiv (as characters), n_non_miss_loci, n_miss_loci (as numeric)
+#' @return returns a data frame with indiv (as characters), n_non_miss_loci, n_miss_loci (as numeric) and missing_loci
+#' (as a list-column of named integer vectors)
 #' @keywords internal
 #' @export
 count_missing_data <- function(D, gen_start_col) {
@@ -17,6 +18,11 @@ count_missing_data <- function(D, gen_start_col) {
   miss <- rowSums(is.na(DM)) / 2
   nonmiss <- rowSums(!is.na(DM)) / 2
 
-  tibble::tibble(indiv = as.character(D$indiv), n_non_miss_loci = nonmiss, n_miss_loci = miss)
+  # finally, let's also store the pattern of missing data in a list column
+  D2 <- D[, seq(1, ncol(D), by = 2)]  # just take the first column for each locus
+  miss_pattern_list <- apply(D2, 1, function(x) which(is.na(x)))
+
+
+  tibble::tibble(indiv = as.character(D$indiv), n_non_miss_loci = nonmiss, n_miss_loci = miss, missing_loci = miss_pattern_list)
 
 }
