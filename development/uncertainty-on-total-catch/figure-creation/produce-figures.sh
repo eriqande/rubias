fc02d8d7473d5bb27d58a3b0d554a52e13792b5e
Eric C. Anderson <eric.anderson@noaa.gov>
2025-08-04 13:43:54 -0600

    Starting work on stock-specific catch uncertainty

---
+++ development/uncertainty-on-total-catch/figure-creation/produce-figures.sh
@@ -0,0 +1,9 @@
+
+FIGS="standard-model"
+
+for i in $FIGS; do
+  pdflatex $i.tex
+  pdfcrop $i.pdf
+  magick -density 600 $i-crop.pdf -background white -alpha remove -alpha off -quality 100 $i-crop.png
+  cp $i-crop.pdf $i-crop.png ../images/
+done
