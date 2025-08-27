2ac587144b816e19d6206f1fad608440afeea21a
Eric C. Anderson <eric.anderson@noaa.gov>
2025-08-27 14:14:04 -0600

    Committing the description

--- development/uncertainty-on-total-catch/figure-creation/produce-figures.sh
+++ development/uncertainty-on-total-catch/figure-creation/produce-figures.sh
@@ -4,6 +4,6 @@ FIGS="standard-model stock-spec-catch-model"
 for i in $FIGS; do
   pdflatex $i.tex
   pdfcrop $i.pdf
-  magick -density 600 $i-crop.pdf -background white -alpha remove -alpha off -quality 100 $i-crop.png
+  magick -density 300 $i-crop.pdf -background white -alpha remove -alpha off -quality 100 $i-crop.png
   cp $i-crop.pdf $i-crop.png ../images/
 done
